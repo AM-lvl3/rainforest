fa7c14eb2fcf93b6bf2f1cbddc57607a5c6e3218
AM-lvl3 <drewmorris@live.com>
2013-11-12 14:35:13 -0500

    writing product validations

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,12 +1,17 @@
 Rainforest::Application.routes.draw do
+  resources :products
+
+  # get 'products' => 'products#index'
+  # get 'products/:id' => 'products#show', as "product"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'products#index'
+
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # get 'products/:id' => 'products#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(
+	:name => "Margaritaville Blender",
+	:description => "For the individual who looks at timeanddate.com to prove it really is 5 o'clock somewhere. 
+	Make perfect margaritas, everytime - Jimmy Buffet style!",
+	:price_in_cents => 19999,
+	:photo => "http://catalog.sears.ca/wcsstore/MasterCatalog/images/catalog/Product_437/std_lang_all/52/_p/611_80152_P.jpg"
+	)
+
+Product.create!(
+	:name => "Sony Walkman",
+	:description => "If you still believe Mr. T is the coolest, Hulkamania will never die, and rock your hair like flock of seagulls, 
+	this portable cassette player is for you!",
+	:price_in_cents => 4599,
+	:photo => "http://upload.wikimedia.org/wikipedia/commons/c/c8/Sony-wm-fx421-walkman.jpg"
+	)
+
+Product.create!(
+	:name => "DOPE ring",
+	:description => "Airport security is whack. 2 chainz wore it, so should you.",
+	:price_in_cents => 4800,
+	:photo => "http://ionehellobeautiful.files.wordpress.com/2012/05/2-chainz-ring.jpg"
+	)
